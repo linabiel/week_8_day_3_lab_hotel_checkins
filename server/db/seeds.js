b6d3f80203a28e67c5ba5556c1e0b65656400afc
linabiel <linuka.b@gmail.com>
2021-05-19 11:59:15 +0100

    db seeded, roures working

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -3,18 +3,18 @@ db.dropDatabase();
 
 db.bookings.insertMany([
     {
-        guest_name: "Yellow Wagtail",
+        guest_name: "Lord Melbury",
         guest_email: "Sutherland",
-        checked_in_status: "2017-06-01"
+        checked_in_status: true
     },
     {
-        guest_name: "Red Kite",
+        guest_name: "Dr Abbot",
         guest_email: "Knockshinnoch",
-        checked_in_status: "2017-01-22"
+        checked_in_status: false
     },
     {
-        guest_name: "Little Egret",
+        guest_name: "Mr Johnson",
         guest_email: "Seamill",
-        checked_in_status: "2018-08-15"
+        checked_in_status: true
     }
 ]);
